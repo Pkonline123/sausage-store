8fcc4fc03107b4bf115c50d25e17638736953c25
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-01-31 15:45:03 +0000

    Update frontend/sausage-store-frontend.service, frontend/deploy.sh, frontend/.gitlab-ci.yml

---
+++ frontend/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Если свалится одна из команд, рухнет и весь скрипт
+set -xe
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+sudo rm -r /var/www-data/dist/frontend||true
+sudo rm -f ./sausage-store-${VERSION}.tar.gz||true
+#Переносим артефакт в нужную папку
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-zachitaylov-andrey-frontend/sausage-store/sausage-store-${VERSION}.tar.gz
+sudo tar xzf sausage-store-${VERSION}.tar.gz -C /var/www-data/dist/frontend||true
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store-frontend 
