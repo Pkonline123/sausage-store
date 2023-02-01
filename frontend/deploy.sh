9cf1362c7f4ee524ef8e7ba65fc6d387902926f0
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-02-01 07:54:19 +0000

    Update frontend/deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,9 +4,9 @@ set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -r /var/www-data/dist/frontend||true
-sudo rm -f ./sausage-store.tar.gz||true
+#sudo rm -f ./sausage-store.tar.gz||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -jlo sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-zachitaylov-andrey-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -JLO sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-zachitaylov-andrey-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar xzf sausage-store.tar.gz -C /var/www-data/dist/||true
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
