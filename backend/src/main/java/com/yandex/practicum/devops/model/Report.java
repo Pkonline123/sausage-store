5def3841e7edc47e500dfb7432f1314cb9d79048
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-03 12:10:43 +0000

    Update backend/src/main/java/com/yandex/practicum/devops/model/Report.java

--- backend/src/main/java/com/yandex/practicum/devops/model/Report.java
+++ backend/src/main/java/com/yandex/practicum/devops/model/Report.java
@@ -1,8 +1,7 @@
 package com.yandex.practicum.devops.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import org.springframework.data.annotation.Id;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
