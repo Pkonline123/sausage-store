4dea415ee7057cf225616a786251710f26f69895
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-16 15:18:47 +0000

    Update backend/Dockerfile, frontend/Dockerfile, backend/.gitlab-ci.yml, backend/backend_deploy.sh

---
+++ backend/backend_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +e
+cat > .env <<EOF
+SPRING_DATASOURCE_URL=${PSQL_DATASOURCE}
+SPRING_DATASOURCE_USERNAME=${PSQL_USER}
+SPRING_DATASOURCE_PASSWORD=${PSQL_PASSWORD}
+SPRING_DATA_MONGODB_URI=${MONGO_DATA}
+EOF
+docker network create -d bridge sausage_network || true
+docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
+docker stop backend || true
+docker rm backend || true
+set -e
+docker run -d --name backend \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    --env-file .env \
+    gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
