d2798645613adff4de45b969fdbf5ae16fc08bba
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-17 08:13:36 +0000

    Update backend/backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -8,6 +8,7 @@ SPRING_DATA_MONGODB_URI=${MONGO_DATA}
 EOF
 docker network rm sausage_network || true
 docker network create -d bridge sausage_network || true
+docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
 docker stop backend || true
 docker rm backend || true
