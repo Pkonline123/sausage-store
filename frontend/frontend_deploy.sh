763670a2379284eed0908f4933fb1db027515698
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-17 15:26:23 +0000

    Update frontend/frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -13,7 +13,7 @@ docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-f
 docker stop frontend || true
 docker rm frontend || true
 set -e
-docker run -d -p 80:8080 --name frontend \
+docker run -d -p 80:80 --name frontend \
     --network=sausage_network \
     --restart always \
     --pull always \
