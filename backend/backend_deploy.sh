5261e7248a33db3f0061bd5d6a57903e94e442bf
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-21 06:52:42 +0000

    Update backend/src/main/resources/application.properties, backend/backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set +e
 cat > .env <<EOF
-SPRING_DATASOURCE_URL='jdbc:postgresql://rc1b-txenjeot0qbcbnh3.mdb.yandexcloud.net:6432/std-013-20'
-SPRING_DATASOURCE_USERNAME='std-013-20'
-SPRING_DATASOURCE_PASSWORD='P@ssw0rd'
-SPRING_DATA_MONGODB_URI='mongodb://std-013-20:P%40ssw0rd@rc1a-xb6q7pmw36t77ryz.mdb.yandexcloud.net:27018/std-013-20?tls=true'
+SPRING_DATASOURCE_URL=jdbc:postgresql://rc1b-txenjeot0qbcbnh3.mdb.yandexcloud.net:6432/std-013-20
+SPRING_DATASOURCE_USERNAME=std-013-20
+SPRING_DATASOURCE_PASSWORD=P@ssw0rd
+SPRING_DATA_MONGODB_URI=mongodb://std-013-20:P%40ssw0rd@rc1a-xb6q7pmw36t77ryz.mdb.yandexcloud.net:27018/std-013-20?tls=true
 EOF
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
