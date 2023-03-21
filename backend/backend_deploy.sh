bf300ac669015ed7bf66d3270caafb99a75e21be
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-21 10:23:49 +0000

    Update backend/backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -5,6 +5,7 @@ SPRING_DATASOURCE_URL=jdbc:postgresql://rc1b-txenjeot0qbcbnh3.mdb.yandexcloud.ne
 SPRING_DATASOURCE_USERNAME=std-013-20
 SPRING_DATASOURCE_PASSWORD=P@ssw0rd
 SPRING_DATA_MONGODB_URI=mongodb://std-013-20:P%40ssw0rd@rc1a-xb6q7pmw36t77ryz.mdb.yandexcloud.net:27018/std-013-20?tls=true
+REPORT_PATH=./logs
 EOF
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
@@ -12,7 +13,7 @@ docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-b
 docker stop backend || true
 docker rm backend || true
 set -e
-docker run -d --name backend \
+docker run -v /home/student/logsBackDocker:./logs -d --name backend \
     --network=sausage_network \
     --restart always \
     --pull always \
