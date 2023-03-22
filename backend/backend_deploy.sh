8cc7e823b2ff6acfd78904336929a9249d77e304
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-03-22 13:39:13 +0000

    Update backend/backend_deploy.sh, backend/.gitlab-ci.yml, frontend/.gitlab-ci.yml, frontend/frontend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set +e
-# cat > .env <<EOF
-#  SPRING_DATASOURCE_URL=${PSQL_DATASOURCE}
-#  SPRING_DATASOURCE_USERNAME=${DATASOURCE_USERNAME}
-#  SPRING_DATASOURCE_PASSWORD=${DATASOURCE_PASSWORD}
-#  SPRING_DATA_MONGODB_URI=${DATA_MONGODB}
-# REPORT_PATH=./logs
-# EOF
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set +e
-cat > .env <<EOF
-SPRING_DATASOURCE_URL=${PSQL_DATASOURCE}
-SPRING_DATASOURCE_USERNAME=${DATASOURCE_USERNAME}
-SPRING_DATASOURCE_PASSWORD=${DATASOURCE_PASSWORD}
-SPRING_DATA_MONGODB_URI=${DATA_MONGODB}
-REPORT_PATH=./logs
-EOF
 # docker network rm sausage_network || true
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
@@ -18,6 +11,5 @@ docker run -d -p 80:80 --name frontend \
     --network=sausage_network \
     --restart always \
     --pull always \
-    --env-file .env \
     gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-frontend:latest
 
