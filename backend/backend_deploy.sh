2fca4a8e1f0e59de706df2fe17dd5386621ac058
Andrey <zachitaylov-l2@yandex.ru>
2023-04-01 12:11:37 +0300

    magec100500

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set +e
+echo SPRING_DATASOURCE_PASSWORD=$(vault kv get -field=spring.datasource.password secret/sausage-store) >> /home/deployservice/env_file
+echo SPRING_DATASOURCE_USERNAME=$(vault kv get -field=spring.datasource.username secret/sausage-store-username) >> /home/deployservice/env_file
+echo SPRING_DATA_MONGODB_URI=$(vault kv get -field=spring.data.mongodb.uri secret/sausage-store-mongo) >> /home/deployservice/env_file
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
