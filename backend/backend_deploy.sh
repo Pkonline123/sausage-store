07b4faadf07f79b71ff4ccc3e13fabba57b8172d
std-013-20 std-013-20 <std-013-20@praktikum-services.ru>
2023-04-01 09:25:56 +0000

    Update backend/backend_deploy.sh, backend/.gitlab-ci.yml

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set +e
-echo SPRING_DATASOURCE_PASSWORD=$(vault kv get -field=spring.datasource.password secret/sausage-store) >> /home/deployservice/env_file
-echo SPRING_DATASOURCE_USERNAME=$(vault kv get -field=spring.datasource.username secret/sausage-store-username) >> /home/deployservice/env_file
-echo SPRING_DATA_MONGODB_URI=$(vault kv get -field=spring.data.mongodb.uri secret/sausage-store-mongo) >> /home/deployservice/env_file
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
 docker stop backend || true
 docker rm backend || true
 set -e
-docker run -v /home/student/logsBackDocker:/app/logs -d --name backend \
+docker run -v /home/student/logsBackDocker:/app/logs -d --name backend --env=${SPRING_DATASOURCE_PASSWORD} --env=${SPRING_DATASOURCE_USERNAME} --env=${SPRING_DATA_MONGODB_URI} \
     --network=sausage_network \
     --restart always \
     --pull always \
