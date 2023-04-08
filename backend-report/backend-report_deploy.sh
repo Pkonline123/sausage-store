0dc9adc62a8bad428bef5ba86f4498fc9cf189df
Andrey <zachitaylov-l2@yandex.ru>
2023-04-08 09:42:07 +0300

    ci

---
+++ backend-report/backend-report_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set +e
+docker network create -d bridge sausage_network || true
+docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/backend-report:latest
+docker compose stop backend-report || true
+docker compose rm backend-report || true
+set -e
+docker compose up backend-report
+# docker run -v /home/student/logsBackDocker:/app/logs -d --name backend-report \
+#     --network=sausage_network \
+#     --restart always \
+#     --pull always \
+#     --env-file ./env_file \
+#     gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/backend-report:latest
