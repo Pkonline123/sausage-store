fcde700ae01344ea881ca22c516d0b7bf07d1c6d
Andrey <zachitaylov-l2@yandex.ru>
2023-04-11 12:04:00 +0300

    test

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -4,7 +4,7 @@ docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/backend-report:latest
 docker-compose stop backend-report || true
-docker-compose rm backend-report || true
+docker-compose rm -f backend-report || true
 set -e
 docker-compose up -d backend-report
 # docker run -v /home/student/logsBackDocker:/app/logs -d --name backend-report \
--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -5,7 +5,7 @@ docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-frontend:latest
 docker-compose stop frontend || true
-docker-compose rm frontend || true
+docker-compose rm -f frontend || true
 set -e
 docker-compose up -d frontend
 # docker run -d -p 80:80 --name frontend \
