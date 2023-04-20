2ab2f368b94efb223dad53a56ad9c937fec1f256
Andrey <zachitaylov-l2@yandex.ru>
2023-04-20 11:10:21 +0300

    deploy

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -2,11 +2,32 @@
 set +e
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
-docker-compose stop backend_blue || true
-docker-compose rm -f backend_blue || true
-set -e
-docker-compose up -d backend_blue
+if [ "$( docker container inspect -f '{{.State.Running}}' blue )" == "true" ]; then
+    docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest    
+    docker-compose stop backend_green || true
+    docker-compose rm -f backend_green || true
+    set -e
+    docker-compose up -d backend_green
+    if [ "$(curl --fail -s http://localhost:8080/actuator/health)" == "healthy" ]; then
+        docker-compose stop backend_blue || true
+        docker-compose rm -f backend_blue || true
+    fi
+elif ["$( docker container inspect -f '{{.State.Running}}' green )" == "true" ]; then
+    docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest    
+    docker-compose stop backend_blue || true
+    docker-compose rm -f backend_blue || true
+    set -e
+    docker-compose up -d backend_blue
+    if [ "$(curl --fail -s http://localhost:8080/actuator/health)" == "healthy" ]; then
+        docker-compose stop backend_green || true
+        docker-compose rm -f backend_green || true
+    fi
+fi
+# docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
+# docker-compose stop backend_blue || true
+# docker-compose rm -f backend_blue || true
+# set -e
+# docker-compose up -d backend_blue
 # docker run -v /home/student/logsBackDocker:/app/logs -d --name backend \
 #     --network=sausage_network \
 #     --restart always \
