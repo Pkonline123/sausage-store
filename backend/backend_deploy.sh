58bb4292c257e32a4093f2f1fc84f630b5934a5b
Andrey <zachitaylov-l2@yandex.ru>
2023-04-20 10:51:02 +0300

    12

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -3,10 +3,10 @@ set +e
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
-docker-compose stop backend || true
-docker-compose rm -f backend || true
+docker-compose stop backend_blue || true
+docker-compose rm -f backend_blue || true
 set -e
-docker-compose up -d backend
+docker-compose up -d backend_blue
 # docker run -v /home/student/logsBackDocker:/app/logs -d --name backend \
 #     --network=sausage_network \
 #     --restart always \
