46f924e705d81be31df161b9829b48511ba04228
Andrey <zachitaylov-l2@yandex.ru>
2023-04-22 13:09:50 +0300

    test

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -8,20 +8,24 @@ if [ "$( docker container inspect -f '{{.State.Running}}' blue )" == "true" ]; t
     docker rm -f green || true
     set -e
     docker-compose up -d backend_green
-    # if [ "$(curl --fail -s http://localhost:8080/actuator/health)" == "healthy" ]; then
-        docker stop blue || true
-        docker rm -f blue || true
-    # fi
+    while [ $(docker exec -it green curl --fail -s http://localhost:8080/actuator/health) == {"status":"UP"} ]
+    do
+            echo unhealthy
+    done
+    docker stop blue || true
+    docker rm -f blue || true
 elif ["$( docker container inspect -f '{{.State.Running}}' green )" == "true" ]; then
     docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest    
     docker stop blue || true
     docker rm -f blue || true
     set -e
     docker-compose up -d backend_blue
-    # if [ "$(curl --fail -s http://localhost:8080/actuator/health)" == "healthy" ]; then
-        docker stop green || true
-        docker rm -f green || true
-    # fi
+    while [ $(docker exec -it green curl --fail -s http://localhost:8080/actuator/health) == {"status":"UP"} ]
+    do
+            echo unhealthy
+    done
+    docker stop green || true
+    docker rm -f green || true
 fi
 # docker pull gitlab.praktikum-services.ru:5050/std-013-20/sausage-store/sausage-backend:latest
 # docker-compose stop backend_blue || true
